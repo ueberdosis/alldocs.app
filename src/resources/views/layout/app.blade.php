9f671f65dce2c15409498ce00ce8582a581cbe6e
Hans Pagel <hans.pagel@ueber.io>
2019-11-21 16:21:55 +0100

    add simple analytics

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -79,5 +79,10 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="{{ mix('/js/app.js') }}"></script>
+
+    @if (config('app.env') === 'production')
+        <script async defer src="https://data.alldocs.app/app.js"></script>
+        <noscript><img src="https://data.alldocs.app/image.gif" alt=""></noscript>
+    @endif
 </body>
 </html>
