d021f8b2214cc6cb9588591d56e3cbb60e55e7db
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-16 16:00:16 +0100

    more styles

--- src/resources/views/components/page-footer/index.blade.php
+++ src/resources/views/components/page-footer/index.blade.php
@@ -1,76 +1,105 @@
 @component('components.section.index')
-  <div>
-    <h3>
-      Alldocs — The text converter for all your documents
-    </h3>
-    <p>
-      This is a super fancy text converter. Wow. Amaze.
-    </p>
-    <h3>
-      Free
-    </h3>
-    <p>
-      We thought about making a VERY expensive app with offline support later, but we’re lazy.
-    </p>
-    <h3>
-      No Ads
-    </h3>
-    <p>
-      We forgot to install ads. Good for you, there is no real tracking here. Sorry, Google.
-    </p>
-    <h3>
-      Secure
-    </h3>
-    <p>
-      After 12 hours your files are deleted from our server. We don’t have time to read them anyway.
-    </p>
-    <h3>
-      Open Startup
-    </h3>
-    <p>
-      We made a public dashboard that has all our numbers. Even the revenue, but surprisingly it’s $0.
-    </p>
-    <h3>
-      Just Works
-    </h3>
-    <p>
-      Upload a file & download the converted file. Like a good joke, it needs no further explanation.
-    </p>
-    <h3>
-      Cool Design
-    </h3>
-    <p>
-      We checked out every post on Dribbble, took the best out and here it is: The perfect design.
-    </p>
+  <div class="grid">
 
-    <ul>
-      <li>
-        <a href="#">
-            Privacy
-        </a>
-      </li>
-      <li>
-        <a href="#">
-            Terms
-        </a>
-      </li>
-      <li>
-        <a href="https://ueber.io/impressum">
-            Impressum
-        </a>
-      </li>
-      <li>
-        <a href="#">
-            About
-        </a>
-      </li>
-      <li>
-        <a href="https://floatie.app/alldocs">
-            Public Metrics
-        </a>
-      </li>
-    </ul>
+    <div class="grid__item">
+      <h2>
+        Alldocs — The text converter for all your documents
+      </h2>
+      <p>
+        This is a super fancy text converter. Wow. Amaze.
+      </p>
+    </div>
 
-    <p>Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a></p>
+    <div class="grid__item">
+      <div class="grid">
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            Free
+          </h3>
+          <p class="small">
+            We thought about making a VERY expensive app with offline support later, but we’re lazy.
+          </p>
+        </div>
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            No Ads
+          </h3>
+          <p class="small">
+            We forgot to install ads. Good for you, there is no real tracking here. Sorry, Google.
+          </p>
+        </div>
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            Secure
+          </h3>
+          <p class="small">
+            After 12 hours your files are deleted from our server. We don’t have time to read them anyway.
+          </p>
+        </div>
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            Open Startup
+          </h3>
+          <p class="small">
+            We made a public dashboard that has all our numbers. Even the revenue, but surprisingly it’s $0.
+          </p>
+        </div>
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            Just Works
+          </h3>
+          <p class="small">
+            Upload a file & download the converted file. Like a good joke, it needs no further explanation.
+          </p>
+        </div>
+
+        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+          <h3>
+            Cool Design
+          </h3>
+          <p class="small">
+            We checked out every post on Dribbble, took the best out and here it is: The perfect design.
+          </p>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="grid__item">
+      <ul class="small">
+        <li>
+          <a href="#">
+              Privacy
+          </a>
+        </li>
+        <li>
+          <a href="#">
+              Terms
+          </a>
+        </li>
+        <li>
+          <a href="https://ueber.io/impressum">
+              Impressum
+          </a>
+        </li>
+        <li>
+          <a href="#">
+              About
+          </a>
+        </li>
+        <li>
+          <a href="https://floatie.app/alldocs">
+              Public Metrics
+          </a>
+        </li>
+      </ul>
+
+      <p>Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a></p>
+    </div>
   </div>
 @endcomponent
