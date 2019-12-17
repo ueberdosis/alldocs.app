f360330881b39fbeae16d06760231ffd9f3c35b1
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-17 09:25:21 +0100

    improve footer

--- src/resources/views/components/page-footer/index.blade.php
+++ src/resources/views/components/page-footer/index.blade.php
@@ -1,105 +1,122 @@
-@component('components.section.index')
-  <div class="grid">
+<div class="c-page-footer">
+  @component('components.section.index')
+    <div class="grid">
 
-    <div class="grid__item">
-      <h2>
-        Alldocs — The text converter for all your documents
-      </h2>
-      <p>
-        This is a super fancy text converter. Wow. Amaze.
-      </p>
-    </div>
+      <div class="grid__item">
+        <h2>
+          Alldocs — The text converter for all your documents
+        </h2>
+        <p>
+          This is a super fancy text converter. Wow. Amaze.
+        </p>
+      </div>
 
-    <div class="grid__item">
-      <div class="grid">
+      <div class="grid__item">
+        <div class="grid">
 
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            Free
-          </h3>
-          <p class="small">
-            We thought about making a VERY expensive app with offline support later, but we’re lazy.
-          </p>
+            <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+              <h3>
+                Free
+              </h3>
+              <p class="small">
+                We thought about making a VERY expensive app with offline support later, but we’re lazy.
+              </p>
+            </div>
+
+            <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+              <h3>
+                No Ads
+              </h3>
+              <p class="small">
+                We forgot to install ads. Good for you, there is no real tracking here. Sorry, Google.
+              </p>
+            </div>
+
+            <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+              <h3>
+                Secure
+              </h3>
+              <p class="small">
+                After 12 hours your files are deleted from our server. We don’t have time to read them anyway.
+              </p>
+            </div>
+
+          <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+            <h3>
+              Open Startup
+            </h3>
+            <p class="small">
+              We made a public dashboard that has all our numbers. Even the revenue, but surprisingly it’s $0.
+            </p>
+          </div>
+
+          <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+            <h3>
+              Just Works
+            </h3>
+            <p class="small">
+              Upload a file & download the converted file. Like a good joke, it needs no further explanation.
+            </p>
+          </div>
+
+          <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
+            <h3>
+              Cool Design
+            </h3>
+            <p class="small">
+              We checked out every post on Dribbble, took the best out and here it is: The perfect design.
+            </p>
+          </div>
+
         </div>
-
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            No Ads
-          </h3>
-          <p class="small">
-            We forgot to install ads. Good for you, there is no real tracking here. Sorry, Google.
-          </p>
-        </div>
-
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            Secure
-          </h3>
-          <p class="small">
-            After 12 hours your files are deleted from our server. We don’t have time to read them anyway.
-          </p>
-        </div>
-
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            Open Startup
-          </h3>
-          <p class="small">
-            We made a public dashboard that has all our numbers. Even the revenue, but surprisingly it’s $0.
-          </p>
-        </div>
-
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            Just Works
-          </h3>
-          <p class="small">
-            Upload a file & download the converted file. Like a good joke, it needs no further explanation.
-          </p>
-        </div>
-
-        <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
-          <h3>
-            Cool Design
-          </h3>
-          <p class="small">
-            We checked out every post on Dribbble, took the best out and here it is: The perfect design.
-          </p>
-        </div>
-
       </div>
     </div>
+  @endcomponent
 
-    <div class="grid__item">
-      <ul class="small">
-        <li>
-          <a href="#">
-              Privacy
-          </a>
-        </li>
-        <li>
-          <a href="#">
-              Terms
-          </a>
-        </li>
-        <li>
-          <a href="https://ueber.io/impressum">
-              Impressum
-          </a>
-        </li>
-        <li>
-          <a href="#">
-              About
-          </a>
-        </li>
-        <li>
-          <a href="https://floatie.app/alldocs">
-              Public Metrics
-          </a>
-        </li>
-      </ul>
+  @component('components.section.index')
+    <div class="c-page-footer__links">
+      <div class="grid" data-grid="narrow" data-grid--medium="normal">
+        <div class="grid__item" data-grid--medium="4/12">
+          <ul class="small">
+            <li>
+              <a href="#">
+                Privacy
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                Terms
+              </a>
+            </li>
+            <li>
+              <a href="https://ueber.io/impressum">
+                Impressum
+              </a>
+            </li>
+          </ul>
+        </div>
 
-      <p>Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a></p>
+        <div class="grid__item" data-grid--medium="4/12">
+          <ul class="small">
+            <li>
+              <a href="#">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="https://floatie.app/alldocs">
+                Public Metrics
+              </a>
+            </li>
+          </ul>
+        </div>
+
+        <div class="grid__item" data-grid--medium="4/12">
+          <small>
+            Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a>
+          </small>
+        </div>
+      </div>
     </div>
-  </div>
-@endcomponent
+  @endcomponent
+</div>
