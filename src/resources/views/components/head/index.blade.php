21602bbb9dffdb1ab7c76fe4b1724227974fd752
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-17 17:23:35 +0100

    add some meta tags

--- src/resources/views/components/head/index.blade.php
+++ src/resources/views/components/head/index.blade.php
@@ -1,14 +1,36 @@
+@php
+$open_graph = [
+	'name' => config('app.name'),
+	'description' => 'The text converter for all your documents.',
+	'url' => Request::fullUrl(),
+	// 'image' => url('assets/images/opengraph.png?' . md5(config('build.number'))),
+];
+@endphp
+
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <meta name="format-detection" content="telephone=no">
 
-<title>{{ config('app.name') }}</title>
+<title>{{ $open_graph['name'] }}</title>
+<meta name="description" content="{{ $open_graph['description'] }}">
 
 @if (manifest('app.css'))
 	<link rel="stylesheet" type="text/css" href="{{ manifest('app.css') }}">
 @endif
+
+<meta property="og:title" content="{{ $open_graph['name'] }}">
+<meta property="og:description" content="{{ $open_graph['description'] }}">
+<meta property="og:url" content="{{ $open_graph['url'] }}">
+{{-- <meta property="og:image" content="{{ $open_graph['image'] }}"> --}}
+<meta property="og:site_name" content="{{ $open_graph['name'] }}">
+
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:url" content="{{ $open_graph['url'] }}">
+<meta name="twitter:title" content="{{ $open_graph['name'] }}">
+<meta name="twitter:description" content="{{ $open_graph['description'] }}">
+{{-- <meta name="twitter:image" content="{{ $open_graph['image'] }}"> --}}
 
 <script>
 	window.app = {!! collect([
