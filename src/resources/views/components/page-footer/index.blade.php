eefd6c01d8358b90baad3884c5e69da57a41f506
Philipp Kühn <kontakt@philipp-kuehn.com>
2019-12-18 11:18:23 +0100

    improve footer again

--- src/resources/views/components/page-footer/index.blade.php
+++ src/resources/views/components/page-footer/index.blade.php
@@ -1,6 +1,6 @@
 <div class="c-page-footer">
   @component('components.section.index', ['color' => 'black'])
-    <div class="c-page-footer__header">
+    <div class="c-page-footer__section">
       <h2>
         Alldocs — The text converter for all your documents
       </h2>
@@ -9,7 +9,7 @@
       </p>
     </div>
 
-    <div class="c-page-footer__content">
+    <div class="c-page-footer__section">
       <div class="grid">
 
         <div class="grid__item" data-grid--small="6/12" data-grid--medium="4/12">
@@ -68,50 +68,50 @@
 
       </div>
     </div>
-  @endcomponent
 
-  @component('components.section.index', ['color' => 'black'])
-    <div class="c-page-footer__links">
-      <div class="grid" data-grid="narrow" data-grid--medium="normal">
-        <div class="grid__item" data-grid--medium="4/12">
-          <ul class="u-small">
-            <li>
-              <a href="#">
-                Privacy
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                Terms
-              </a>
-            </li>
-            <li>
-              <a href="https://ueber.io/impressum">
-                Impressum
-              </a>
-            </li>
-          </ul>
-        </div>
+    <div class="c-page-footer__section">
+      <div class="c-page-footer__links">
+        <div class="grid" data-grid="narrow" data-grid--medium="normal">
+          <div class="grid__item" data-grid--medium="4/12">
+            <ul class="u-small">
+              <li>
+                <a href="#">
+                  Privacy
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  Terms
+                </a>
+              </li>
+              <li>
+                <a href="https://ueber.io/impressum">
+                  Impressum
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        <div class="grid__item" data-grid--medium="4/12">
-          <ul class="u-small">
-            <li>
-              <a href="#">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="https://floatie.app/alldocs">
-                Public Metrics
-              </a>
-            </li>
-          </ul>
-        </div>
+          <div class="grid__item" data-grid--medium="4/12">
+            <ul class="u-small">
+              <li>
+                <a href="#">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="https://floatie.app/alldocs">
+                  Public Metrics
+                </a>
+              </li>
+            </ul>
+          </div>
 
-        <div class="grid__item" data-grid--medium="4/12">
-          <small>
-            Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a>
-          </small>
+          <div class="grid__item" data-grid--medium="4/12">
+            <small>
+              Made with 💙 by <a href="https://twitter.com/_ueberdosis">überclub</a>
+            </small>
+          </div>
         </div>
       </div>
     </div>
