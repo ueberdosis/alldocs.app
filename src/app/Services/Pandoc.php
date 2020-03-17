513d934d79a8c917152f89c5b9a2ddb6fb3da3e7
Hans Pagel <hans.pagel@ueber.io>
2020-03-17 15:16:13 +0100

    refactor

--- src/app/Services/Pandoc.php
+++ src/app/Services/Pandoc.php
@@ -128,17 +128,19 @@ abstract class Pandoc
 
     public static function convert($file, $from, $to, $outputFile = null)
     {
-        $inputFile = storage_path('app/public/').$file;
+        $inputFile = storage_path("app/public/{$file}");
 
         if (!$outputFile) {
-            $outputFile = storage_path('app/public/').explode('.', $file)[0].'.'.$to;
+            $outputFilename = explode('.', $file)[0].'.'.$to;
+            $outputFile = storage_path("app/public/{$outputFilename}");
         } else {
-            $outputFile = storage_path('app/public/').$outputFile;
+            $outputFile = storage_path("app/public/{$outputFile}");
         }
 
         // Example: pandoc test1.md -f markdown -t html -s -o test1.html
         $command = sprintf("pandoc %s -f %s -t %s -s -o %s", $inputFile, $from, $to, $outputFile);
         exec(escapeshellcmd($command), $cmdOutput);
+
         return $cmdOutput;
     }
 }
