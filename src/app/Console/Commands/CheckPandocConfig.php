d4fc3ccce3b7e834f29db32be363eda1929a9680
Hans Pagel <hans.pagel@ueber.io>
2020-03-17 14:01:11 +0100

    add title to config check

--- src/app/Console/Commands/CheckPandocConfig.php
+++ src/app/Console/Commands/CheckPandocConfig.php
@@ -44,28 +44,29 @@ class CheckPandocConfig extends Command
         $formatsTotalStatus = $formatsTotal ? '✅' : '❌';
         $this->info("{$formatsTotalStatus} Found {$formatsTotal} formats.");
 
+        $formatsWithTitle = $formats->whereNotNull('title')->count();
+        $formatsWithTitlePercentage = number_format(100/$formatsTotal * $formatsWithTitle, 1);
+        $formatsWithTitleStatus = $formatsWithTitle === $formatsTotal ? '✅' : '❌';
+        $this->info("{$formatsWithTitleStatus} {$formatsWithTitlePercentage} % ({$formatsWithTitle}/{$formatsTotal}) have a title.");
+
         $formatsWithLongTitle = $formats->whereNotNull('long_title')->count();
         $formatsWithLongTitlePercentage = number_format(100/$formatsTotal * $formatsWithLongTitle, 1);
         $formatsWithLongTitleStatus = $formatsWithLongTitle === $formatsTotal ? '✅' : '❌';
-
         $this->info("{$formatsWithLongTitleStatus} {$formatsWithLongTitlePercentage} % ({$formatsWithLongTitle}/{$formatsTotal}) have a long title.");
 
         $formatsWithDescription = $formats->whereNotNull('description')->count();
         $formatsWithDescriptionPercentage = number_format(100/$formatsTotal * $formatsWithDescription, 1);
         $formatsWithDescriptionStatus = $formatsWithDescription === $formatsTotal ? '✅' : '❌';
-
         $this->info("{$formatsWithDescriptionStatus} {$formatsWithDescriptionPercentage} % ({$formatsWithDescription}/{$formatsTotal}) have a description.");
 
         $formatsWithDefaultExtension = $formats->whereNotNull('default_extension')->count();
         $formatsWithDefaultExtensionPercentage = number_format(100/$formatsTotal * $formatsWithDefaultExtension, 1);
         $formatsWithDefaultExtensionStatus = $formatsWithDefaultExtension === $formatsTotal ? '✅' : '❌';
-
         $this->info("{$formatsWithDefaultExtensionStatus} {$formatsWithDefaultExtensionPercentage} % ({$formatsWithDefaultExtension}/{$formatsTotal}) have a default extension.");
 
         $formatsWithExtensions = $formats->whereNotNull('extensions')->count();
         $formatsWithExtensionsPercentage = number_format(100/$formatsTotal * $formatsWithExtensions, 1);
         $formatsWithExtensionsStatus = $formatsWithExtensions === $formatsTotal ? '✅' : '❌';
-
         $this->info("{$formatsWithExtensionsStatus} {$formatsWithExtensionsPercentage} % ({$formatsWithExtensions}/{$formatsTotal}) have allowed extensions.");
     }
 }
