aefc376f72427f05330453ae9533292d89fac006
Hans Pagel <hans.pagel@ueber.io>
2021-02-02 17:29:05 +0100

    add plausible

--- src/resources/views/components/footer/index.blade.php
+++ src/resources/views/components/footer/index.blade.php
@@ -1,6 +1,7 @@
 @if (config('app.env') === 'production')
   <script async defer src="https://data.alldocs.app/app.js"></script>
   <noscript><img src="https://data.alldocs.app/image.gif" alt=""></noscript>
+  <script async defer data-domain="alldocs.app" src="https://plausible.io/js/plausible.js"></script>
 @endif
 
 @if (manifest('vendor.js'))
